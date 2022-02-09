741db45db44c957a5f5ab62641c8d3c252bcf111
mirogaudi <mirogaudi@ya.ru>
2022-02-09 20:50:38 +0100

    Add tests

---
+++ src/test/java/mirogaudi/productcatalog/ProductCatalogServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package mirogaudi.productcatalog;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class ProductCatalogServiceApplicationTest {
+
+    @Test
+    void main() {
+        assertDoesNotThrow(() ->
+                ProductCatalogServiceApplication.main(new String[]{})
+        );
+    }
+
+}
