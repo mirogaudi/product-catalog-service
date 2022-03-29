a24395d727990ea4e0ccf40840a5595bd6054b19
mirogaudi <mirogaudi@ya.ru>
2022-03-29 21:12:25 +0200

    Add .editorconfig, clean up DB scripts

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -1,25 +1,18 @@
 /* Categories */
 insert into category (name, parent_id)
-values ('Computers', null);
-insert into category (name, parent_id)
-values ('Portable computers', (select id from category where name = 'Computers'));
-insert into category (name, parent_id)
-values ('Tablets', (select id from category where name = 'Portable computers'));
-insert into category (name, parent_id)
-values ('Apple', null);
+values ('Computers', null),
+       ('Portable computers', (select id from category where name = 'Computers')),
+       ('Tablets', (select id from category where name = 'Portable computers')),
+       ('Apple', null);
 
 /* Products */
 insert into product (name, price, currency, original_price, original_currency)
-values ('MacBook', 1677.55, 'EUR', 1999, 'USD');
-insert into product (name, price, currency, original_price, original_currency)
-values ('iPad', 838.99, 'EUR', 999.99, 'USD');
+values ('MacBook', 1677.55, 'EUR', 1999, 'USD'),
+       ('iPad', 838.99, 'EUR', 999.99, 'USD');
 
 /* Product Categories */
 insert into product_category (product_id, category_id)
-values ((select id from product where name = 'MacBook'), (select id from category where name = 'Portable computers'));
-insert into product_category (product_id, category_id)
-values ((select id from product where name = 'MacBook'), (select id from category where name = 'Apple'));
-insert into product_category (product_id, category_id)
-values ((select id from product where name = 'iPad'), (select id from category where name = 'Tablets'));
-insert into product_category (product_id, category_id)
-values ((select id from product where name = 'iPad'), (select id from category where name = 'Apple'));
+values ((select id from product where name = 'MacBook'), (select id from category where name = 'Portable computers')),
+       ((select id from product where name = 'MacBook'), (select id from category where name = 'Apple')),
+       ((select id from product where name = 'iPad'), (select id from category where name = 'Tablets')),
+       ((select id from product where name = 'iPad'), (select id from category where name = 'Apple'));
