c994fe1de538dc49b7029774b9b2e11efba9caf4
mirogaudi <mirogaudi@ya.ru>
2023-02-20 13:37:44 +0100

    Fix springdoc configuration

--- src/main/java/mirogaudi/productcatalog/config/SwaggerConfig.java
+++ src/main/java/mirogaudi/productcatalog/config/SwaggerConfig.java
@@ -2,9 +2,7 @@ package mirogaudi.productcatalog.config;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,13 +15,7 @@ public class SwaggerConfig {
             .info(new Info()
                 .title("Product Catalog API")
                 .description("Demo product catalog service")
-                .version("v1.0.0")
-                .contact(new Contact()
-                    .email("mirogaudi@ya.ru")
-                    .url("https://github.com/mirogaudi"))
-                .license(new License()
-                    .name("Apache-2.0")
-                    .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .version("v1.0.0"))
             .externalDocs(new ExternalDocumentation()
                 .description("Source code on GitHub")
                 .url("https://github.com/mirogaudi/product-catalog-service"));
