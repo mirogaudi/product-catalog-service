600516834b5ef21958f9ee1869726c61fae331a2
mirogaudi <mirogaudi@ya.ru>
2024-02-06 16:06:48 +0100

    Clean up maven build

--- src/main/java/mirogaudi/productcatalog/connector/ConnectorRuntimeException.java
+++ src/main/java/mirogaudi/productcatalog/connector/ConnectorRuntimeException.java
@@ -1,9 +1,14 @@
 package mirogaudi.productcatalog.connector;
 
+import java.io.Serial;
+
 /**
  * Connector runtime exception.
  */
 public class ConnectorRuntimeException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public ConnectorRuntimeException(String message, Throwable cause) {
         super(message, cause);
