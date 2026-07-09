3dd9fcf025eb5878ea89515e53a153008deb75c8
mirogaudi <mirogaudi@ya.ru>
2026-07-09 15:15:21 +0200

    Clean up code

--- src/main/java/mirogaudi/productcatalog/config/CacheConfig.java
+++ src/main/java/mirogaudi/productcatalog/config/CacheConfig.java
@@ -2,10 +2,12 @@ package mirogaudi.productcatalog.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,32 +15,40 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.Duration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @EnableCaching
 @EnableScheduling
 @Slf4j
 public class CacheConfig {
 
-    public static final String RATES_CACHE_NAME = "rates";
+    public static final String RATES_CACHE_NAME = "rates-cache";
+
+    @Value("${pcs.cache.enabled:false}")
+    private boolean cacheEnabled;
 
     @Bean
     public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager(RATES_CACHE_NAME);
-        cacheManager.setCaffeine(Caffeine.newBuilder()
-            .expireAfterWrite(Duration.ofHours(24)) // safety fallback in case cron based eviction fails
-            .maximumSize(500) // prevents unbounded growth
-            .evictionListener((key, _, cause) ->
-                LOG.info("Cache({}): key {} was evicted ({})", RATES_CACHE_NAME, key, cause))
-            .removalListener((key, _, cause) ->
-                LOG.info("Cache({}): key {} was removed ({})", RATES_CACHE_NAME, key, cause))
-            .recordStats()
-        );
-        return cacheManager;
+        if (cacheEnabled) {
+            CaffeineCacheManager cacheManager = new CaffeineCacheManager(RATES_CACHE_NAME);
+            cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofHours(24)) // safety fallback in case cron based eviction fails
+                .maximumSize(500) // prevents unbounded growth
+                .evictionListener((key, _, cause) ->
+                    LOG.info("Cache({}): key {} was evicted ({})", RATES_CACHE_NAME, key, cause))
+                .removalListener((key, _, cause) ->
+                    LOG.info("Cache({}): key {} was removed ({})", RATES_CACHE_NAME, key, cause))
+                .recordStats()
+            );
+            return cacheManager;
+        } else {
+            return new NoOpCacheManager();
+        }
     }
 
+    // TODO add test!
     @Scheduled(
-        cron = "${pcs.rates-cache-evict.cron}",
-        zone = "${pcs.rates-cache-evict.zone}"
+        cron = "${pcs.cache.rates-cache.evict.cron}",
+        zone = "${pcs.cache.rates-cache.evict.zone}"
     )
     @CacheEvict(value = RATES_CACHE_NAME, allEntries = true)
     public void evictRatesCache() {
