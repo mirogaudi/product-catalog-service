9685cc86600260a25be8ab5fa3faf3cb37cdb1dc
mirogaudi <mirogaudi@ya.ru>
2026-07-31 11:13:37 +0200

    Use explicit test profile in spring boot tests

--- src/test/java/mirogaudi/productcatalog/repository/CategoryRepositoryIntegrationTest.java
+++ src/test/java/mirogaudi/productcatalog/repository/CategoryRepositoryIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 import java.util.Set;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase.Replace.NONE;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = NONE)
 class CategoryRepositoryIntegrationTest {
 
--- src/test/java/mirogaudi/productcatalog/repository/ProductRepositoryIntegrationTest.java
+++ src/test/java/mirogaudi/productcatalog/repository/ProductRepositoryIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase.Replace.NONE;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = NONE)
 class ProductRepositoryIntegrationTest {
 
